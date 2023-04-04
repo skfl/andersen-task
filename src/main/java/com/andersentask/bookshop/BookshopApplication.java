5bac77dc97db3759b1cc61474b7bc989ec69fb8e
ulad-sachkovski <118623122+ulad-sachkovski@users.noreply.github.com>
2023-04-04 21:22:21 +0400

    Revert "Feature: Book entity and service implementation"

--- src/main/java/com/andersentask/bookshop/BookshopApplication.java
+++ src/main/java/com/andersentask/bookshop/BookshopApplication.java
@@ -3,8 +3,6 @@ package com.andersentask.bookshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
 @SpringBootApplication
 public class BookshopApplication {
 
