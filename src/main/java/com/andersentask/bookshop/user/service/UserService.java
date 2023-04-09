026d79ad83b6f9c8d7714432f78bb1690ee9f728
DenisBratuh <denis.bratuh@gmail.com>
2023-04-09 13:54:22 +0300

    Feature: "User" Description: minor fixes

--- src/main/java/com/andersentask/bookshop/user/service/UserService.java
+++ src/main/java/com/andersentask/bookshop/user/service/UserService.java
@@ -3,27 +3,24 @@ package com.andersentask.bookshop.user.service;
 import com.andersentask.bookshop.user.entities.User;
 import com.andersentask.bookshop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
-@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
 
     public boolean registration(User user) {
         if (userRepository.findByEmailIgnoreCase(user.getEmail()).isPresent()) {
-            log.info("User already exists with email: {}", user.getEmail());
             return false;
         }
         userRepository.save(user);
-        log.info("Successfully registered! Your email: {}; password: {}", user.getEmail(), user.getPassword());
         return true;
     }
 
-    public User findByEmail(String email) {
-        return userRepository.findByEmailIgnoreCase(email)
-                .orElseThrow(() -> new RuntimeException("User is missing with email:" + email));
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmailIgnoreCase(email);
     }
 
 }
