0a96a20942cc62c48543d89acdeb640ece459ffa
ulad-sachkovski <118623122+ulad-sachkovski@users.noreply.github.com>
2023-04-10 12:20:42 +0400

    add unit-tests

---
+++ src/test/java/com/andersentask/bookshop/order/service/OrderServiceTest.java
@@ -0,0 +1,44 @@
+package com.andersentask.bookshop.order.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void saveOrder() {
+    }
+
+    @Test
+    void saveOrdersFromListOfRequests() {
+    }
+
+    @Test
+    void completeOrder() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void getIncomeForPeriod() {
+    }
+
+    @Test
+    void getOrdersSortedByCost() {
+    }
+
+    @Test
+    void getOrdersSortedByDate() {
+    }
+
+    @Test
+    void getOrdersSortedByStatus() {
+    }
+}
