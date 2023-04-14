4eb1964c2d17dfb29e4c46a2d54de9b0b9438eb8
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-14 12:41:59 +0400

    change initial method name; remove unnecessary space;

--- src/main/java/com/andersentask/bookshop/BookshopApplication.java
+++ src/main/java/com/andersentask/bookshop/BookshopApplication.java
@@ -6,9 +6,7 @@ import com.andersentask.bookshop.console.ConsoleAppController;
 public class BookshopApplication {
 
     public static void main(String[] args) {
-
         ConsoleAppController consoleAppController = new ConsoleAppController();
-
-        consoleAppController.init();
+        consoleAppController.run();
     }
 }
