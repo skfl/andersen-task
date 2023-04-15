426953fc92b84127c79f037a042269ea3abbec87
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-15 23:10:59 +0400

    add test servlet class

---
+++ src/main/java/com/andersentask/bookshop/http/servlets/TestServlet.java
@@ -0,0 +1,17 @@
+package com.andersentask.bookshop.http.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("HELLO WORLD");
+    }
+}
