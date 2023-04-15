aec67815e006831d0c2bef2f32016f1e2c6b1dbe
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-15 15:02:42 +0400

    remove useless lombok annotations; remove value field in OrderStatus

--- src/main/java/com/andersentask/bookshop/book/enums/BookStatus.java
+++ src/main/java/com/andersentask/bookshop/book/enums/BookStatus.java
@@ -1,10 +1,5 @@
 package com.andersentask.bookshop.book.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum BookStatus {
     AVAILABLE,
     OUT_OF_STOCK
--- src/main/java/com/andersentask/bookshop/order/enums/OrderStatus.java
+++ src/main/java/com/andersentask/bookshop/order/enums/OrderStatus.java
@@ -1,14 +1,7 @@
 package com.andersentask.bookshop.order.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum OrderStatus {
-    IN_PROCESS("IN_PROCESS"),
-    COMPLETED("COMPLETED"),
-    CANCELED("CANCELED");
-
-    private final String value;
+    IN_PROCESS,
+    COMPLETED,
+    CANCELED;
 }
