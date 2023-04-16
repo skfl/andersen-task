b67b9642852a102c21cc8fb23be6c0bb07793341
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-16 19:15:53 +0400

    add JsonServlet class to work  with http request; add some servlet classes to implement functionality of application

---
+++ src/main/java/com/andersentask/bookshop/http/servlets/CreateOrderServlet.java
@@ -0,0 +1,22 @@
+package com.andersentask.bookshop.http.servlets;
+
+import com.andersentask.bookshop.console.Commands;
+
+import java.util.List;
+import java.util.Map;
+
+public class CreateOrderServlet extends JsonServlet {
+
+    private final Commands commands;
+
+    public CreateOrderServlet(Commands commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    Response post(String uri, Map<String, List<String>> body) {
+        return new Response(commands.createOrder(body.get("ids").stream()
+                .map(Long::parseLong)
+                .toList()));
+    }
+}
---
+++ src/main/java/com/andersentask/bookshop/http/servlets/GetRequestsServlet.java
@@ -0,0 +1,19 @@
+package com.andersentask.bookshop.http.servlets;
+
+import com.andersentask.bookshop.console.Commands;
+
+import java.util.Map;
+
+public class GetRequestsServlet extends JsonServlet {
+
+    private final Commands commands;
+
+    public GetRequestsServlet(Commands commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    Response get(String uri, Map<String, String[]> parameters) {
+        return new Response(commands.getAllRequests());
+    }
+}
