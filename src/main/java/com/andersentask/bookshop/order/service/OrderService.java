89a72431c7ac19c9e9667b59355c7d71f25792cb
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-20 15:14:59 +0400

    implement OrderRepository JDBC

--- src/main/java/com/andersentask/bookshop/order/service/OrderService.java
+++ src/main/java/com/andersentask/bookshop/order/service/OrderService.java
@@ -8,7 +8,9 @@ import com.andersentask.bookshop.order.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,38 +31,37 @@ public class OrderService {
         return orderRepository.findAll();
     }
 
-    public void changeStatusOfOrder(Long id, OrderStatus orderStatus) {
-        Optional<Order> optionalOrder = getOrderById(id);
-        if (optionalOrder.isPresent()) {
-            Order order = optionalOrder.get();
-
-            switch (orderStatus) {
-                case COMPLETED -> {
-                    if (order.getOrderStatus() == OrderStatus.IN_PROCESS) {
-                        order.setOrderStatus(OrderStatus.COMPLETED);
-                        order.setTimeOfCompletingOrder(LocalDateTime.now());
-                    }
-                }
-                case CANCELED -> {
-                    if (order.getOrderStatus() == OrderStatus.IN_PROCESS) {
-                        order.setOrderStatus(OrderStatus.CANCELED);
-                    }
-                }
-            }
+    public Order changeStatusOfOrder(Long orderId, OrderStatus orderStatus) {
+        Order orderToUpdate = orderRepository.findById(orderId).orElseThrow();
+        if (fromInProcessToCompleted(orderToUpdate, orderStatus)) {
+            orderToUpdate.setOrderStatus(OrderStatus.COMPLETED);
+            orderToUpdate.setTimeOfCompletingOrder(Timestamp.valueOf(LocalDateTime.now()));
+            orderRepository.update(orderToUpdate);
         }
+        if (fromInProcessToCanceled(orderToUpdate, orderStatus)) {
+            orderToUpdate.setOrderStatus(OrderStatus.CANCELED);
+            orderRepository.update(orderToUpdate);
+        }
+        return orderToUpdate;
     }
 
+    private boolean fromInProcessToCompleted(Order orderToUpdate, OrderStatus orderStatus) {
+        return orderToUpdate.getOrderStatus() == OrderStatus.IN_PROCESS
+                && orderStatus == OrderStatus.COMPLETED;
+    }
+
+    private boolean fromInProcessToCanceled(Order orderToUpdate, OrderStatus orderStatus) {
+        return orderToUpdate.getOrderStatus() == OrderStatus.IN_PROCESS
+                && orderStatus == OrderStatus.CANCELED;
+    }
+
+
     public List<Order> getSortedOrders(OrderSort orderSort) {
-        return orderRepository.getSortedOrders(orderSort);
+        return orderRepository.findSortedOrders(orderSort);
     }
 
     public BigDecimal getIncomeForPeriod(LocalDateTime startOfPeriod, LocalDateTime endOfPeriod) {
-        return getAllOrders().stream()
-                .filter(order -> order.getOrderStatus() == OrderStatus.COMPLETED)
-                .filter(order -> order.getTimeOfCompletingOrder().isAfter(startOfPeriod) &&
-                        order.getTimeOfCompletingOrder().isBefore(endOfPeriod))
-                .map(Order::getOrderCost)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return orderRepository.findIncomeForPeriod(startOfPeriod, endOfPeriod);
     }
 
     public List<Book> getAllBooksFromOrder(Long id) {
