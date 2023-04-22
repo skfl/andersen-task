849d0f4a2b32039d7f4b6cd3718682a01ae853f8
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-22 18:12:46 +0400

    add jakarta entity annotations to Book entity

--- src/main/java/com/andersentask/bookshop/book/entities/Book.java
+++ src/main/java/com/andersentask/bookshop/book/entities/Book.java
@@ -1,6 +1,14 @@
 package com.andersentask.bookshop.book.entities;
 
 import com.andersentask.bookshop.book.enums.BookStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +20,21 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "status")
+    @Enumerated(value = EnumType.STRING)
     private BookStatus status;
 
+    @Column(name = "price")
     private BigDecimal price;
 }
