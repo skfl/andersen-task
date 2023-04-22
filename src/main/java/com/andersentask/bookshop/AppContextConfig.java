900a692e14b29bf104ea6547afcecc5d8194128b
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-22 18:11:10 +0400

    add hibernate configuration to AppContextConfig

--- src/main/java/com/andersentask/bookshop/AppContextConfig.java
+++ src/main/java/com/andersentask/bookshop/AppContextConfig.java
@@ -1,14 +1,13 @@
 package com.andersentask.bookshop;
 
+import com.andersentask.bookshop.book.entities.Book;
 import com.andersentask.bookshop.book.repositories.BookRepository;
 import com.andersentask.bookshop.book.services.BookService;
 import com.andersentask.bookshop.broker.EntityFactory;
-import com.andersentask.bookshop.order.repositories.OrderRepository;
 import com.andersentask.bookshop.order.service.OrderService;
-import com.andersentask.bookshop.request.repository.RequestRepository;
 import com.andersentask.bookshop.request.services.RequestService;
 import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManager;
 import liquibase.Contexts;
 import liquibase.LabelExpression;
 import liquibase.Liquibase;
@@ -18,36 +17,34 @@ import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
-import java.sql.SQLException;
+import java.util.Properties;
 
 @Slf4j
 public class AppContextConfig {
 
-    public static final String ORG_POSTGRESQL_DRIVER = "org.postgresql.Driver";
-
-    private static final String DB_USER = "postgres";
-
-    private static final String DB_PASSWORD = "31150616";
-
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/bookstore";
-
     private final BookService bookService;
 
-    private final OrderService orderService;
+    private OrderService orderService;
 
-    private final RequestService requestService;
+    private  RequestService requestService;
 
     private final EntityFactory entityFactory;
 
-    private final HikariDataSource dataSource;
+    private final SessionFactory sessionFactory;
 
     public AppContextConfig() {
-        dataSource = new HikariDataSource(getHikariConfig());
+        sessionFactory = getHibernateSessionFactory();
+        EntityManager entityManager =  sessionFactory.createEntityManager();
         liquibase();
-        this.bookService = new BookService(new BookRepository(dataSource));
-        this.requestService = new RequestService(new RequestRepository(dataSource, bookService));
-        this.orderService = new OrderService(new OrderRepository(dataSource, bookService));
+        this.bookService = new BookService(new BookRepository(entityManager));
+//        this.requestService = new RequestService(new RequestRepository(dataSource, bookService));
+//        this.orderService = new OrderService(new OrderRepository(dataSource, bookService));
         this.entityFactory = new EntityFactory();
     }
 
@@ -67,29 +64,46 @@ public class AppContextConfig {
         return entityFactory;
     }
 
-    private HikariConfig getHikariConfig() {
-        HikariConfig config = new HikariConfig();
-        config.setUsername(DB_USER);
-        config.setPassword(DB_PASSWORD);
-        config.setDriverClassName(ORG_POSTGRESQL_DRIVER);
-        config.setJdbcUrl(DB_URL);
-        config.setMaximumPoolSize(20);
-        return config;
+    private void liquibase() {
+        sessionFactory.getCurrentSession().beginTransaction();
+        sessionFactory.getCurrentSession().doWork(connection -> {
+            connection.setAutoCommit(false);
+            try (Database database = DatabaseFactory.getInstance()
+                    .findCorrectDatabaseImplementation(new JdbcConnection(connection))) {
+                Liquibase liquibase = new Liquibase
+                        ("db/changelog/db.changelog-master.yaml", new ClassLoaderResourceAccessor(), database);
+                liquibase.update(new Contexts(), new LabelExpression());
+                connection.commit();
+            } catch (LiquibaseException e) {
+                log.error(e.getMessage());
+                connection.rollback();
+                throw new IllegalArgumentException(e);
+            }
+        });
     }
 
-    private void liquibase() {
-        try (Database database = DatabaseFactory.getInstance()
-                .findCorrectDatabaseImplementation(new JdbcConnection(dataSource.getConnection()))) {
-            Liquibase liquibase = new liquibase
-                    .Liquibase
-                    ("db/changelog/db.changelog-master.yaml", new ClassLoaderResourceAccessor(), database);
-            liquibase.update(new Contexts(), new LabelExpression());
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new IllegalArgumentException("Can't apply liquibase");
-        } catch (LiquibaseException e) {
-            log.error(e.getMessage());
-            throw new IllegalArgumentException(e);
-        }
+    private SessionFactory getHibernateSessionFactory() {
+
+        Configuration configuration = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(AvailableSettings.DRIVER, "org.postgresql.Driver");
+        properties.put(AvailableSettings.URL, "jdbc:postgresql://localhost:5432/bookstore");
+        properties.put(AvailableSettings.USER, "postgres");
+        properties.put(AvailableSettings.PASS, "31150616");
+        properties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put(AvailableSettings.SHOW_SQL, "true");
+        properties.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.put(AvailableSettings.HBM2DDL_AUTO, "none");
+        properties.put(AvailableSettings.CONNECTION_PROVIDER, "com.zaxxer.hikari.hibernate.HikariConnectionProvider");
+        properties.put(AvailableSettings.POOL_SIZE, 20);
+
+        configuration.setProperties(properties);
+        configuration.addAnnotatedClass(Book.class);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        return configuration.buildSessionFactory(serviceRegistry);
     }
 }
