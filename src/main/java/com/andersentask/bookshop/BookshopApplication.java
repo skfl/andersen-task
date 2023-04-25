d6b4255a00640069eeef5b4d9d359b8de43e4b3f
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-04-25 23:22:32 +0400

    add spring boot dependency;

--- src/main/java/com/andersentask/bookshop/BookshopApplication.java
+++ src/main/java/com/andersentask/bookshop/BookshopApplication.java
@@ -1,11 +1,12 @@
 package com.andersentask.bookshop;
 
-import com.andersentask.bookshop.http.HttpInterface;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BookshopApplication {
 
     public static void main(String[] args) {
-        HttpInterface httpInterface = new HttpInterface();
-        httpInterface.start();
+        SpringApplication.run(BookshopApplication.class, args);
     }
 }
