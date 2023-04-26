78ae1e4855a1f443e5a11ffa02aa5d47f10db3aa
ulad-sachkovski <118623122+ulad-sachkovski@users.noreply.github.com>
2023-04-26 15:24:48 +0400

    move book controller to ReponseEntity<>

--- src/main/java/com/andersentask/bookshop/controllers/BookController.java
+++ src/main/java/com/andersentask/bookshop/controllers/BookController.java
@@ -4,10 +4,12 @@ import com.andersentask.bookshop.book.entities.Book;
 import com.andersentask.bookshop.book.enums.BookSort;
 import com.andersentask.bookshop.book.enums.BookStatus;
 import com.andersentask.bookshop.broker.Commands;
-import com.andersentask.bookshop.broker.enums.ResultOfOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -19,13 +21,18 @@ public class BookController {
 
     private final Commands commands;
 
-    @GetMapping("/all")
-    public List<Book> getBookList(@RequestParam(name = "sort", defaultValue = "ID") String sort) {
-        return commands.getSortedBooks(BookSort.valueOf(sort.toUpperCase(Locale.ROOT)));
+    @GetMapping("all")
+    public ResponseEntity<List<Book>> getBookList(@RequestParam(name = "sort", defaultValue = "ID") String sort) {
+        return new ResponseEntity<>(
+                commands.getSortedBooks(BookSort.valueOf(sort.toUpperCase(Locale.ROOT))),
+                HttpStatus.OK);
     }
 
-    @PostMapping("/{bookId}/status")
-    public ResultOfOperation.SetBookStatus changeBookStatus(@PathVariable Long bookId, @RequestBody Map<String, BookStatus> body) {
-        return commands.setStatusToBookAndDeleteCorrespondingRequests(bookId, body.get("status"));
+    @PostMapping("{bookId}/status")
+    public ResponseEntity<Map<String, String>> changeBookStatus(@PathVariable Long bookId, @RequestBody Map<String, BookStatus> body) {
+        Map<String, String> response = new HashMap<>();
+        String result = commands.setStatusToBookAndDeleteCorrespondingRequests(bookId, body.get("status")).toString();
+        response.put("operationResult", result);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
