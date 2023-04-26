acc5cdcf8788471ad90cc1aae9fe439353962552
ulad-sachkovski <118623122+ulad-sachkovski@users.noreply.github.com>
2023-04-26 20:33:01 +0400

    move request controller to ResponseEntity<>

--- src/main/java/com/andersentask/bookshop/controllers/RequestController.java
+++ src/main/java/com/andersentask/bookshop/controllers/RequestController.java
@@ -1,14 +1,15 @@
 package com.andersentask.bookshop.controllers;
 
 import com.andersentask.bookshop.broker.Commands;
-import com.andersentask.bookshop.broker.enums.ResultOfOperation;
 import com.andersentask.bookshop.request.entities.Request;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/request")
@@ -18,22 +19,29 @@ public class RequestController {
     private final Commands commands;
 
     @GetMapping("all")
-    public List<Request> getRequestList() {
-        return commands.getAllRequests();
+    public ResponseEntity<List<Request>> getRequestList() {
+        return new ResponseEntity<>(commands.getAllRequests()
+                , HttpStatus.OK);
     }
 
     @GetMapping("map")
-    public Map<Long, Long> getBooksAndNumberOfRequests() {
-        return commands.getBooksAndNumberOfRequests();
+    public ResponseEntity<Map<Long, Long>> getBooksAndNumberOfRequests() {
+        return new ResponseEntity<>(commands.getBooksAndNumberOfRequests(),
+                HttpStatus.OK);
     }
 
     @PostMapping("new")
-    public ResultOfOperation.CreateRequest createRequest(@RequestBody Map<String, Long> body) {
-        return commands.createRequest(body.get("bookId"));
+    public ResponseEntity<Map<String,String>> createRequest(@RequestBody Map<String, Long> body) {
+        Map<String, String> response = new HashMap<>();
+        String result = commands.createRequest(body.get("bookId"))
+                .toString();
+        response.put("operationResult", result);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("book/{id}")
-    public Optional<Long> getNumberOfRequestsOnBook(@PathVariable Long id) {
-        return commands.getNumberOfRequestsOnBook(id);
+    public ResponseEntity<Long> getNumberOfRequestsOnBook(@PathVariable Long id) {
+        return new ResponseEntity<>(commands.getNumberOfRequestsOnBook(id),
+                HttpStatus.OK);
     }
 }
